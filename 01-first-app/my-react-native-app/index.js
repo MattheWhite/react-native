6c13b2028dd7f7e924fdd79c9e04cb11b52a1aef
MattheWhite <fehermatyas97@gmail.com>
2025-03-12 16:51:05 +0100

    Add comment line in index.js with command to create a blank base app without TypeScript

--- 01-first-app/my-react-native-app/index.js
+++ 01-first-app/my-react-native-app/index.js
@@ -2,6 +2,8 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+// npx create-expo-app --template blank   -> command to create a blank expo driven app
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
