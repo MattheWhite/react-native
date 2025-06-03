9135d0541ebb01f547b655e70060a6e64d88f010
MattheWhite <fehermatyas97@gmail.com>
2025-06-03 16:48:50 +0200

    Add explanation in index.js enterpoint file

--- react-js-refresher/src/index.js
+++ react-js-refresher/src/index.js
@@ -11,6 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
+// ReactDOM.render(<App />, document.getElementById('root')); => React element/component's name always has to start with capital letter, this is what tells React it is a custom component which it has to render, not a default built-in one
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
