08dddfbc5b45e10df4adae10421ef543d68bcc14
MattheWhite <fehermatyas97@gmail.com>
2025-06-05 16:54:52 +0200

    Modify GoalList.js component, add key prop for React tracking the elements

--- react-js-refresher/src/components/GoalList.js
+++ react-js-refresher/src/components/GoalList.js
@@ -1,6 +1,6 @@
 import React from "react"; // always necessary, without this we can't use JSX syntax
 
-import './GoalList.css'; // styling import, separated only for leaner code. CSS always applied globally no matter where you import them, applied to the whole page
+import "./GoalList.css"; // styling import, separated only for leaner code. CSS always applied globally no matter where you import them, applied to the whole page
 
 /* 
 Different ways to define a component
@@ -9,12 +9,17 @@ function GoalList
 const GoalList = function GoalList ...
 const GoalList = () => {}
  */
-const GoalList = (props) => { // every function used by React which returns JSX will receive parameters/props automatically
+const GoalList = (props) => {
+  // every function used by React which returns JSX will receive parameters/props automatically
   // return statement in JSX code looks like this, prettier structuring with using () brackets
   console.log(props.goals); // props is an Object
   return (
-    <ul className="goal-list"> {/* class is a JS attribute keyword so JSX uses className */}
-      {[<li>HI</li>, <li>This works</li>]}
+    <ul className="goal-list">
+      {/* class is a JS attribute keyword so JSX uses className */}
+      {/* {[<li>HI</li>, <li>This works</li>]} */}
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;/* key prop can be tracked by React */
+      })}
     </ul>
   );
 };
