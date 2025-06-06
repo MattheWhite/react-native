847675f9e15aa0a28a1f062ecc778dd073b0eb97
MattheWhite <fehermatyas97@gmail.com>
2025-06-06 16:23:06 +0200

    Implement basic logic for new goal component

--- react-js-refresher/src/components/NewGoal/NewGoal.js
+++ react-js-refresher/src/components/NewGoal/NewGoal.js
@@ -3,7 +3,18 @@ import React from "react";
 import './NewGoal.css';
 
 const NewGoal = () => {
-    return <form>
+    const addGoalHandler = event => {/* will get event automatically, passed by React to the function */
+        event.preventDefault();
+
+        const newGoal = {
+            id: Math.random().toString(),
+            text: 'My new Goal!'
+        };
+
+        console.log(newGoal);
+    };
+    
+    return <form className="new-goal" onSubmit={addGoalHandler}>{/* only pass the function's pointer! no execution () here, React will handle the execution automatically */}
         <input type="text" />
         <button type="submit" >Add Goal</button>
     </form>
