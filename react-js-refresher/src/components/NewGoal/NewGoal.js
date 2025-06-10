7803d0c5be6791169016a0cabedc0a91a0fe2aca
MattheWhite <fehermatyas97@gmail.com>
2025-06-10 17:29:14 +0200

    Implement using Input and Two-Way binding for handle the input state

--- react-js-refresher/src/components/NewGoal/NewGoal.js
+++ react-js-refresher/src/components/NewGoal/NewGoal.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './NewGoal.css';
 
 const NewGoal = (props) => {/* since now in App.js we pass onAddGoal prop to this component, we can expecting here props contains something -> React always passing props, worst case it's empty */
+    const [enteredText, setEnteredText] = useState('');
     const addGoalHandler = event => {/* will get event automatically, passed by React to the function */
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My new Goal!'
+            text: enteredText
         };
 
+        console.log(newGoal);
+
         props.onAddGoal(newGoal); // passing the newGoal dummy data to the funciton reference/pointer
+    };
+
+    const textChangeHandler = event => {
+        setEnteredText(event.target.value); // event will hold the entered value of the input HTMLElement
     };
     
     return <form className="new-goal" onSubmit={addGoalHandler}>{/* only pass the function's pointer! no execution () here, React will handle the execution automatically
     FUN FACT: basically all is props, just one receives an Object (props), other receives a pointer to a function (event handler) */}
-        <input type="text" />
+        <input type="text" value={enteredText} onChange={textChangeHandler} />{/* binding the changeEvent and the value too, so it reflects on every change, but for that a simple variable binding wont work, because that state are not handled so we have to use USESTATE hook to handle state */}
         <button type="submit" >Add Goal</button>
     </form>
 }
