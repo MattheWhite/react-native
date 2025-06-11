cc3fdc0ead39a3a9f8bd79fbeb7420a2cd214f05
MattheWhite <fehermatyas97@gmail.com>
2025-06-11 14:09:15 +0200

    Add step-by-step in App.js to resolve the starting error, update package.json and add the lock file too

--- 02-react-native-basics/App.js
+++ 02-react-native-basics/App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/* 
+Outdated package.json
+Fix:  1. step:  ncu -u
+      2. step: npm i
+      3. step: npm start > Press a (for Android)
+        on the emulated device Press Ctrl + M  to open developer menu
+*/
+
 export default function App() {
   return (
     <View style={styles.container}>
