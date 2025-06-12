3ddce800b15cd0758db413cfebd726bbbeb3d06a
MattheWhite <fehermatyas97@gmail.com>
2025-06-12 16:12:13 +0200

    Create and add a new plain App.js for the next application declaration

---
+++ 02-react-native-basics/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { View } from "react-native-web";
+
+export default function App() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+
+});
