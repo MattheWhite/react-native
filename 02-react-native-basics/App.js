f1c95cb2866136b63c6cf2cde13e957a4620a6da
MattheWhite <fehermatyas97@gmail.com>
2025-06-12 16:41:08 +0200

    Using flexbox to create base layouts

--- 02-react-native-basics/App.js
+++ 02-react-native-basics/App.js
@@ -3,8 +3,8 @@ import { Button, StyleSheet, TextInput, View, Text } from "react-native";
 export default function App() {
     return (
         <View style={style.appContainer}>
-            <View>
-                <TextInput placeholder="Your course goal here" />
+            <View style={style.inputContainer}>
+                <TextInput style={style.textInput} placeholder="Your course goal here" />
                 <Button title="Add Goal" />
             </View>
             <View>
@@ -17,5 +17,16 @@ export default function App() {
 const style = StyleSheet.create({
     appContainer: {
         padding: 50
+    },
+    inputContainer: {
+        flexDirection: "row", // default setting is 'column' -> thats why it is displayed below eachother at start 
+        justifyContent: "space-between"
+    },
+    textInput: {
+        borderWidth: 2,
+        borderColor: '#cccc',
+        width: '80%',
+        marginRight: 8, // no touching between the input and the button
+        padding: 8 // placeholder not be on the left edge
     }
 });
