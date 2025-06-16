9daa1ed122ca170b35e4bf914bccc3bd445cb3ea
MattheWhite <fehermatyas97@gmail.com>
2025-06-16 16:53:38 +0200

    Implement stylng for the app

--- 02-react-native-basics/App.js
+++ 02-react-native-basics/App.js
@@ -7,7 +7,7 @@ export default function App() {
                 <TextInput style={style.textInput} placeholder="Your course goal here" />
                 <Button title="Add Goal" />
             </View>
-            <View>
+            <View style={style.goalsContainer}>
                 <Text>List of goals...</Text>
             </View>
         </View>
@@ -16,11 +16,18 @@ export default function App() {
 
 const style = StyleSheet.create({
     appContainer: {
-        padding: 50
+        flex: 1, // assign the whole available space for the app, since it has no sibling element, with 1 it takes all the available space
+        paddingTop: 50,
+        paddingHorizontal: 16
     },
     inputContainer: {
+        flex: 1,// flexbox functionality used with this property, learned from Flexbox section, by default React Native uses flexbox on elements
         flexDirection: "row", // default setting is 'column' -> thats why it is displayed below eachother at start 
-        justifyContent: "space-between"
+        justifyContent: "space-between",
+        alignItems: 'center',
+        marginottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccc'
     },
     textInput: {
         borderWidth: 2,
@@ -28,5 +35,8 @@ const style = StyleSheet.create({
         width: '80%',
         marginRight: 8, // no touching between the input and the button
         padding: 8 // placeholder not be on the left edge
+    },
+    goalsContainer: {
+        flex: 8
     }
 });
