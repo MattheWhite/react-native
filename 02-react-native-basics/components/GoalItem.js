2a50681b64dac7dec814890779098492ad594b6a
MattheWhite <fehermatyas97@gmail.com>
2025-06-20 16:47:37 +0200

    Implementing remove actin with .bind()

--- 02-react-native-basics/components/GoalItem.js
+++ 02-react-native-basics/components/GoalItem.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View, Pressable } from "react-native";
 
 // create standard React component, modern React don't need anymore "import React from react" !
 function GoalItem(props) {
-  function deleteGoalHandler() {
+/*   function deleteGoalHandler() {   implementing .bind() built-in JS funciton which we can setup here the will be called binded function's behavior (like passing parameters to it)
     props.onDeleteGoal(props.id);
-  }
+  } */
 
   return (
-    <Pressable onPress={deleteGoalHandler}>
+    <Pressable onPress={props.onDeleteGoal.bind(this, props.id)}>{/* onPress={deleteGoalHandler} prev. solution, now using .bind() to setup here the called function */}
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
