a276fe9148a69dff978a375de188ac357867d3d4
MattheWhite <fehermatyas97@gmail.com>
2025-06-24 17:03:05 +0200

    Implementing special styling effect for IOS too

--- 02-react-native-basics/components/GoalItem.js
+++ 02-react-native-basics/components/GoalItem.js
@@ -12,6 +12,7 @@ function GoalItem(props) {
       <Pressable
         onPress={props.onDeleteGoal.bind(this, props.id)}
         android_ripple={{ color: "#210664" }}
+       style={({pressed}) => {pressed && styles.pressedItem}} /* style can take a function alternatively, this function will be called automatically by Pressable when 'pressed' state changes! You will get the parameter automatically, here we use OBJECT DESTRUCTURING = the same as we would declare: (pressData) => {pressData.pressed} but this way we directly access the property. If pressed we return the pressItem styling, otherwise nothing => this whole declaration only for IOS */
       >{/* enabling ripple effect on adnroid. Moving PRESSABLE element inside the View the effect is shown on the whole object not just around it */}
         {/* onPress={deleteGoalHandler} prev. solution, now using .bind() to setup here the called function */}
         <Text style={styles.goalText}>{props.text}</Text>
@@ -27,6 +28,9 @@ const styles = StyleSheet.create({
     margin: 8, // outer spacing
     borderRadius: 6,
     backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5
   },
   goalText: {
       color: "white",
