86e87254c144704c40d0f4ddd7031c88c57350e7
MattheWhite <fehermatyas97@gmail.com>
2025-06-24 17:13:52 +0200

    Reformat GoalItem.js

--- 02-react-native-basics/components/GoalItem.js
+++ 02-react-native-basics/components/GoalItem.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, Modal } from "react-native";
 // Pressable -> makes a component pressable,   OLD: Touchable, TouchableHighlight, etc.  will be deprecated prossibly soon
 
 // create standard React component, modern React don't need anymore "import React from react" !
@@ -8,16 +8,21 @@ function GoalItem(props) {
   } */
 
   return (
-    <View style={styles.goalItem}>
-      <Pressable
-        onPress={props.onDeleteGoal.bind(this, props.id)}
-        android_ripple={{ color: "#210664" }}
-       style={({pressed}) => {pressed && styles.pressedItem}} /* style can take a function alternatively, this function will be called automatically by Pressable when 'pressed' state changes! You will get the parameter automatically, here we use OBJECT DESTRUCTURING = the same as we would declare: (pressData) => {pressData.pressed} but this way we directly access the property. If pressed we return the pressItem styling, otherwise nothing => this whole declaration only for IOS */
-      >{/* enabling ripple effect on adnroid. Moving PRESSABLE element inside the View the effect is shown on the whole object not just around it */}
-        {/* onPress={deleteGoalHandler} prev. solution, now using .bind() to setup here the called function */}
-        <Text style={styles.goalText}>{props.text}</Text>
-      </Pressable>
-    </View>
+    <Modal>
+      <View style={styles.goalItem}>
+        <Pressable
+          onPress={props.onDeleteGoal.bind(this, props.id)}
+          android_ripple={{ color: "#210664" }}
+          style={({ pressed }) => {
+            pressed && styles.pressedItem;
+          }} /* style can take a function alternatively, this function will be called automatically by Pressable when 'pressed' state changes! You will get the parameter automatically, here we use OBJECT DESTRUCTURING = the same as we would declare: (pressData) => {pressData.pressed} but this way we directly access the property. If pressed we return the pressItem styling, otherwise nothing => this whole declaration only for IOS */
+        >
+          {/* enabling ripple effect on adnroid. Moving PRESSABLE element inside the View the effect is shown on the whole object not just around it */}
+          {/* onPress={deleteGoalHandler} prev. solution, now using .bind() to setup here the called function */}
+          <Text style={styles.goalText}>{props.text}</Text>
+        </Pressable>
+      </View>
+    </Modal>
   );
 }
 
@@ -30,10 +35,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#5e0acc",
   },
   pressedItem: {
-    opacity: 0.5
+    opacity: 0.5,
   },
   goalText: {
-      color: "white",
-      padding: 8, // inner spacing
+    color: "white",
+    padding: 8, // inner spacing
   },
 });
