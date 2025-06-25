71d9cbc773d97057b2d41b490267ab9b1bc51b4c
MattheWhite <fehermatyas97@gmail.com>
2025-06-25 12:45:01 +0200

    Add cancel button and more styling

--- 02-react-native-basics/components/GoalInput.js
+++ 02-react-native-basics/components/GoalInput.js
@@ -16,7 +16,8 @@ function GoalInput(props) {
   }
 
   return (
-    <Modal visible={props.visible} animationType="slide">{/* visible and animationType is Modal properties, props.visible is mine */}
+    <Modal visible={props.visible} animationType="slide">
+      {/* visible and animationType is Modal properties, props.visible is mine */}
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
@@ -26,9 +27,12 @@ function GoalInput(props) {
             enteredGoalText
           } /* add two-way binding since we set back to empty string manually after adding the goal */
         />
-        {/* only the pointer passed, no ()parentheses at the end because then when the code is compiled/rendered it would be executed */}
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+        <View>
+          {/* only the pointer passed, no ()parentheses like:addGoalHandler() at the end because then when the code is compiled/rendered it would be executed */}
+          <Button title="Add Goal" onPress={addGoalHandler} />
+          <Button title="Cancel" />
+        </View>
+      </View>{/* if you move this over the View which contains the Buttons i think looks dope */}
     </Modal>
   );
 }
@@ -38,8 +42,8 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1, // flexbox functionality used with this property, learned from Flexbox section, by default React Native uses flexbox on elements
-    flexDirection: "row", // default setting is 'column' -> thats why it is displayed below eachother at start
-    justifyContent: "space-between",
+    // flexDirection: "row", // default setting is 'column' -> thats why it is displayed below eachother at start
+    justifyContent: "center", // space-between previously
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
