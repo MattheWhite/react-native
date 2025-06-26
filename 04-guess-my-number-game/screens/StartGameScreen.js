19834e4769c98e36fad45bc47c2dca5bbae3f0a7
MattheWhite <fehermatyas97@gmail.com>
2025-06-26 17:14:47 +0200

    Implement shadow styling for both Android and IOS

--- 04-guess-my-number-game/screens/StartGameScreen.js
+++ 04-guess-my-number-game/screens/StartGameScreen.js
@@ -15,8 +15,20 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    // flex: 1,  element takes as much space as possible
     marginTop: 100,
+    marginHorizontal: 24,
     padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // box shadow effect on Android ONLY  |  can't be transformed into a Native element which IOS can understand too  => use shadow...  properties for IOS
+    /*  
+    ONLY FOR IOS
+    
+    shadowColor: "black",
+    shadowOffset: { width: 3, height: 3 },
+    shadowRadius: 6,
+    shadowOpacity: 1
+*/
   },
 });
