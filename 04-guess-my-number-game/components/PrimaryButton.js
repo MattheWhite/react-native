ec96458fa13e2e212107e399bf0a3985e1d84bdc
MattheWhite <fehermatyas97@gmail.com>
2025-06-27 12:27:04 +0200

    Implement styling for iOS devices too, add more explanation and example for style prop

--- 04-guess-my-number-game/components/PrimaryButton.js
+++ 04-guess-my-number-game/components/PrimaryButton.js
@@ -4,8 +4,11 @@ function PrimaryButton(props) {
   /* or use OBJECT DESTRUCTURING => PrimaryButton({children}) */
   return (
     <View style={styles.buttonOuterContainer}>{/* we have to apply here the text styling, since in RN styling there is NO INHERITANCE */}
-      <Pressable style={styles.buttonInnerContainer} android_ripple={{ color: "#da5698" }}>
-        <Text style={styles.buttonText}>{props.children}</Text>
+      <Pressable
+        style={({pressed}) => pressed ? [styles.iOSPressedStyling, styles.buttonInnerContainer] : styles.buttonInnerContainer} // automatically called on press action and passed the 'pressed' boolean data
+        android_ripple={{ color: "#da5698" }}
+      >
+        <Text style={[styles.buttonText]}>{props.children}</Text>{/* styles CAN RECEIVE ARRAY of styles, which all will be applied | ARROW FUNCTION which will be executed */}
       </Pressable>
     </View>
   );
@@ -28,5 +31,8 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     textAlign: "center",
+  },
+  iOSPressedStyling: {
+    opacity: 0.75,
   },
 });
