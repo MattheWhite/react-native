4aaaec9e91d57e0accbca054f224fd2dc12966d0
MattheWhite <fehermatyas97@gmail.com>
2025-06-27 11:32:05 +0200

    Restructure PrimaryButton components styling

--- 04-guess-my-number-game/components/PrimaryButton.js
+++ 04-guess-my-number-game/components/PrimaryButton.js
@@ -3,23 +3,26 @@ import { View, Text, Pressable, StyleSheet } from "react-native";
 function PrimaryButton(props) {
   /* or use OBJECT DESTRUCTURING => PrimaryButton({children}) */
   return (
-    <Pressable>
-      <View style={styles.buttonContainer}>{/* we have to apply here the text styling, since in RN styling there is NO INHERITANCE */}
+    <View style={styles.buttonOuterContainer}>{/* we have to apply here the text styling, since in RN styling there is NO INHERITANCE */}
+      <Pressable style={styles.buttonInnerContainer} android_ripple={{ color: "#da5698" }}>
         <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: "#72063c",
+  buttonOuterContainer: {
     borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
     paddingVertical: 8, // adding different spacing left-right then top-bottom
     paddingHorizontal: 16,
-    margin: 4,
     elevation: 2,
   },
   buttonText: {
