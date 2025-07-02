34733460be0fbd0eaace6b3f247fdc29c79577fb
MattheWhite <fehermatyas97@gmail.com>
2025-07-02 16:46:25 +0200

    Implement resetInputHandler logic

--- 04-guess-my-number-game/screens/StartGameScreen.js
+++ 04-guess-my-number-game/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
@@ -9,12 +9,20 @@ function StartGameScreen() {
     setEnteredNumber(enteredText);
   }
 
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
   function confirmInputHandler() {
     const chosenNumber = parseInt(enteredNumber);
 
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-        // show alert  ...
-        return;
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]/* configuring the Button displayed, the style can be: destructive, cancel, default, onPress we can pass a pointer which will be executed */
+      );
+      return;
     }
   }
 
@@ -34,7 +42,7 @@ function StartGameScreen() {
       <View style={styles.buttonsContainer}>
         {/* every View constructs a new flexbox container */}
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
