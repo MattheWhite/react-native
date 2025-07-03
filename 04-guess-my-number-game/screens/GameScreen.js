7ae092f39b1c58929bf97fa389b507c72412dbbc
MattheWhite <fehermatyas97@gmail.com>
2025-07-03 15:33:04 +0200

    Implement random number generation algorithm

--- 04-guess-my-number-game/screens/GameScreen.js
+++ 04-guess-my-number-game/screens/GameScreen.js
@@ -1,11 +1,26 @@
 import { View, StyleSheet } from "react-native";
 
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min; // adding min so can't be 0 worst case scenario
+
+  if (rndNum === exclude) {
+    // defending that the device can't win at start
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber); // 100 because the upper boundary is excluded, userNumber so the device can't guess the inputNumber at rendering
+  const [currentGuess, setCurrentGuess] = useState();
+
   return (
     <View style={styles.screen}>
-        <Title>Opponent's Guess</Title>
+      <Title>Opponent's Guess</Title>
     </View>
   );
 }
@@ -15,6 +30,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding:24,
+    padding: 24,
   },
 });
