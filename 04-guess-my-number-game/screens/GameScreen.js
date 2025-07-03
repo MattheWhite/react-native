7d4d711adf1314998eabac2b44842e25cb01e2f5
MattheWhite <fehermatyas97@gmail.com>
2025-07-03 10:35:29 +0200

    Format the GameScreen title

--- 04-guess-my-number-game/screens/GameScreen.js
+++ 04-guess-my-number-game/screens/GameScreen.js
@@ -3,13 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess!</Text>
-      {/* GUESS */}
-      <View>
-        <Text>Higher or lower?</Text>
-        {/* + - */}
-      </View>
-      {/* <View>LOG ROUNDS</View> */}
+      <Text style={styles.title}>Opponent's Guess!</Text>
     </View>
   );
 }
@@ -19,6 +13,15 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 20,
+    padding:24,
   },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12
+  }
 });
