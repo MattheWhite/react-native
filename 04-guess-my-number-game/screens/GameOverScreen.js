4a0cdc4b8d034512831f435575498cbe3972a1a9
MattheWhite <fehermatyas97@gmail.com>
2025-07-04 16:31:28 +0200

    Add success image source file and display it on the winning screen

--- 04-guess-my-number-game/screens/GameOverScreen.js
+++ 04-guess-my-number-game/screens/GameOverScreen.js
@@ -1,24 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 import Colors from "../constants/colors";
+import Title from "../components/ui/Title";
 
 function GameOverScreen() {
-    return <View style={styles.container}>
-         <Text style={styles.text}>Game is over!</Text>
-        </View>
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Game is over!</Text>
+      </View>
+      <View>
+        <Title>Game Over</Title>
+        <Image source={require('../assets/images/success.png')} />
+      </View>
+    </>
+  );
 }
 
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    text: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: Colors.accent500,
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: Colors.accent500,
+  },
 });
