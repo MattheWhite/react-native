3792c5ef7f92f23faf6babc03e3c6ffa0f7c1b6e
MattheWhite <fehermatyas97@gmail.com>
2025-07-04 17:04:31 +0200

    Styling further the GameOver screen

--- 04-guess-my-number-game/screens/GameOverScreen.js
+++ 04-guess-my-number-game/screens/GameOverScreen.js
@@ -5,21 +5,32 @@ import Title from "../components/ui/Title";
 
 function GameOverScreen() {
   return (
-    <>
-      <View style={styles.container}>
+    /* <View style={styles.container}>
         <Text style={styles.text}>Game is over!</Text>
+      </View> */
+
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
       </View>
-      <View>
-        <Title>Game Over</Title>
-        <Image source={require('../assets/images/success.png')} />
-      </View>
-    </>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
   );
 }
 
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
     alignItems: "center",
@@ -29,5 +40,20 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     color: Colors.accent500,
+  },
+  imageContainer: {
+    // borderRadius should be half of the width and height value, thats how we create a square and then round it for a circle
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary700,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    // the percentage always refer to the container which contains the image in RN!
+    width: "100%",
+    height: "100%",
   },
 });
