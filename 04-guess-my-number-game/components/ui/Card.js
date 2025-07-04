10f92630ab981e493294cc6b4a1115ba786f9c65
MattheWhite <fehermatyas97@gmail.com>
2025-07-04 11:36:32 +0200

    Add styling for Card component from StartGameScreen component

--- 04-guess-my-number-game/components/ui/Card.js
+++ 04-guess-my-number-game/components/ui/Card.js
@@ -1,7 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-function Card() {}
+function Card({ children }) { // now with children incoming parameter we can use this component as a wrapper
+  return <View style={styles.card}>{children}</View>;
+}
 
 export default Card;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    alignItems: "center",
+    marginTop: 36,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4,
+
+    /* iOS  */
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  },
+});
