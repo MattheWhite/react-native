0e4453773d940d07c320ccdf8c0dc0df10abf401
MattheWhite <fehermatyas97@gmail.com>
2025-07-04 12:34:35 +0200

    Implementing style inheritance with style parameter acceptance, add explanation for it in InstructionText component

--- 04-guess-my-number-game/components/ui/InstructionText.js
+++ 04-guess-my-number-game/components/ui/InstructionText.js
@@ -2,8 +2,8 @@ import { Text, StyleSheet } from "react-native";
 
 import Colors from "../../constants/colors";
 
-function InstructionText({ children }) {
-  return <Text style={styles.instructionText}>{children}</Text>;
+function InstructionText({ children, style }) { // styling inheritance implementation in RN, style prop of components accept an array[] too! the array containing the style objects are evaluated from left-to-right, so the last styles can overwrite the earlier objects if there is overlapping
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 export default InstructionText;
--- 04-guess-my-number-game/screens/GameScreen.js
+++ 04-guess-my-number-game/screens/GameScreen.js
@@ -10,8 +10,7 @@ import Card from "../components/ui/Card";
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min; // adding min so can't be 0 worst case scenario
 
-  if (rndNum === exclude) {
-    // defending that the device can't win at start
+  if (rndNum === exclude) {// defending that the device can't win at start
     return generateRandomBetween(min, max, exclude);
   } else {
     return rndNum;
@@ -25,12 +24,13 @@ function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber); // here we hardcode so the final guess won't crack the code on rerender, we only need this for once anyway
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
-  useEffect(() => { // use this effect and executes if one of the passed dependency/state is changed
+  useEffect(() => {
+    // use this effect and executes if one of the passed dependency/state is changed
     if (currentGuess === userNumber) {
-        onGameOver();
+      onGameOver();
     }
   }, [currentGuess, userNumber, onGameOver]); // simple rule, all the variables and functions which are used should be passed here to watch
-  
+
   function nextGuessHandler(direction) {
     // 'lower', 'greater'
     if (
@@ -47,7 +47,11 @@ function GameScreen({ userNumber, onGameOver }) {
     } else {
       minBoundary = currentGuess + 1;
     }
-    const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
     setCurrentGuess(newRndNumber);
   }
 
@@ -56,14 +60,18 @@ function GameScreen({ userNumber, onGameOver }) {
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
-        <InstructionText>Higher or lower?</InstructionText>
-        <View>
-          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-            -
-          </PrimaryButton>
-          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-            +
-          </PrimaryButton>
+        <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+              -
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+              +
+            </PrimaryButton>
+          </View>
         </View>
       </Card>
     </View>
@@ -76,5 +84,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+  },
+  instructionText: {
+    marginBottom: 12
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
