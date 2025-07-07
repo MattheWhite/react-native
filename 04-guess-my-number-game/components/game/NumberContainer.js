c1e116226c75ed1be17f3bcc63dc4728bd1116b0
MattheWhite <fehermatyas97@gmail.com>
2025-07-07 17:15:33 +0200

    Using Dimensions API for dynamic size settings according to the device

--- 04-guess-my-number-game/components/game/NumberContainer.js
+++ 04-guess-my-number-game/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 import Colors from "../../constants/colors";
 
@@ -12,19 +12,22 @@ function NumberContainer({ children }) { // children arrives from between this c
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get('window').width;
+// we can receive the Device's dimension values before the runApplication is called so before the app is starts | 'screen' OR 'window' => only matters for Android, on iOS no difference, on Android 'screen' means the entire available space including the status bar on top, 'window' only the usable space without the status bar on top
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 400 ? 12 : 24, // dynamic fontsize, padding and margin according to the device size
+    margin: deviceWidth < 400 ? 12 : 24,
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 400? 28 : 36,
     // fontWeight: "bold",
     fontFamily: 'open-sans-bold'
   },
