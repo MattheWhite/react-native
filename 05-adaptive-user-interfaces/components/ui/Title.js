43c82de6d8cd38cfa710613113d1ce3cb25516c3
MattheWhite <fehermatyas97@gmail.com>
2025-07-07 16:57:48 +0200

    Adapting width dynamically in Title.js

--- 05-adaptive-user-interfaces/components/ui/Title.js
+++ 05-adaptive-user-interfaces/components/ui/Title.js
@@ -16,5 +16,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
+    maxWidth: '80%', // max width adapting (relative) to the parent element, always refer to the parent container which holds Title in this case so the VIew element in GameScreen => if less is enough then won't use the whole space, this is the MAX it can use, but can use less
+    minWidth: 300 // must use, this is the MINIMUM space, it only can be bigger BUT not bigger then the '80%'
   },
 });
