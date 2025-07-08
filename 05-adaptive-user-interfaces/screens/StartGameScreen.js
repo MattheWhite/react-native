cd30a1796843c7909db2e51af1d0e591123e886e
MattheWhite <fehermatyas97@gmail.com>
2025-07-08 14:20:38 +0200

    Set size dynamically for different orientation using built-in hook

--- 05-adaptive-user-interfaces/screens/StartGameScreen.js
+++ 05-adaptive-user-interfaces/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert, Dimensions } from 'react-native';
+import { TextInput, View, StyleSheet, Alert, useWindowDimensions } from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
@@ -9,6 +9,8 @@ import InstructionText from '../components/ui/InstructionText';
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  const { widht, height } = useWindowDimensions(); // built-in hook for dynamic style changing, retrieves an object, watches the device's dimensions if the device rotated, automatically executes this hook and updates the object - use object destruction
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -33,8 +35,10 @@ function StartGameScreen({ onPickNumber }) {
     onPickNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 440 ? 50 : 85; // we merge this styling into the components style={} because this part of the code gets reevaluating, but the StyleSheet.create() only once and then that is loaded
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, {marginTop: marginTopDistance} ]}>
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>
@@ -64,12 +68,19 @@ function StartGameScreen({ onPickNumber }) {
 
 export default StartGameScreen;
 
+/*
+MOVE the dynamic screen syling to the component's function because that is executed multiple times and we have a special built-in hook for that!
+Hooks only can be used inside the component's funciton or inside another hook!
+Because from here, because this part of the code only executed once!
+
 const deviceHeight = Dimensions.get('window').height;
+
+*/
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: deviceHeight < 440 ? 50 : 80,
+    // marginTop: deviceHeight < 440 ? 50 : 80,
     alignItems: 'center',
   },
   numberInput: {
