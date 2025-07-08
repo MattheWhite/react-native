6a48555d38354b87a33da8d0f2d96ce9576a6c1c
MattheWhite <fehermatyas97@gmail.com>
2025-07-08 16:34:54 +0200

    Add cleaner solution using Platform.select()

--- 05-adaptive-user-interfaces/components/ui/Title.js
+++ 05-adaptive-user-interfaces/components/ui/Title.js
@@ -13,7 +13,10 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
-    borderWidth: Platform.OS === 'android' ? 0 : 2, // the Platform lives through the lifecycle of the app since it won't change where it runs | making different design depending on the OS it runs on
+    
+    //borderWidth: Platform.OS === 'android' ? 0 : 2, the Platform lives through the lifecycle of the app since it won't change where it runs | making different design depending on the OS it runs on
+    borderWidth: Platform.select({android: 0, ios: 2}), // other solution without ternary operator, cleaner code
+
     borderColor: 'white',
     padding: 12,
     maxWidth: '80%', // max width adapting (relative) to the parent element, always refer to the parent container which holds Title in this case so the VIew element in GameScreen => if less is enough then won't use the whole space, this is the MAX it can use, but can use less
