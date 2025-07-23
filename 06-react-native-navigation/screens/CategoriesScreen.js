eff3deea5c53f35b491ae78ceb71338e5f28ee12
MattheWhite <fehermatyas97@gmail.com>
2025-07-23 16:59:32 +0200

    Modify CategoryScreen.js

--- 06-react-native-navigation/screens/CategoriesScreen.js
+++ 06-react-native-navigation/screens/CategoriesScreen.js
@@ -1,7 +1,13 @@
+import { FlatList } from 'react-native';
+
 import { CATEGORIES } from '../data/dummy-data';
 
+function renderCategoryItem(item) {// optional, outsource here for cleaner code
+    return;
+}
+
 function CategoriesScreen() {
-    return
+    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={} />{/* keyExtractor automatically gets item */}
 }
 
 export default CategoriesScreen;
