70f874083410f6980a4867583889ee3136fbbde0
MattheWhite <fehermatyas97@gmail.com>
2025-07-28 16:54:21 +0200

    Implement basic navigation container

--- 06-react-native-navigation/App.js
+++ 06-react-native-navigation/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; // should wrap around all containers/components which we want to use the benefits of navigation package
 
 import CategoriesScreen from './screens/CategoriesScreen';
 
@@ -12,8 +13,10 @@ import CategoriesScreen from './screens/CategoriesScreen';
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style='light' />{/* on statusBar we won't use navigation but on the whole app we will */}
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
   );
 }
