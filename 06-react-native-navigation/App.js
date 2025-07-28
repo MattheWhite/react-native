366eec4e21ea3dc51391e2862c58f2d691fd9338
MattheWhite <fehermatyas97@gmail.com>
2025-07-28 17:39:57 +0200

    Add main basic navigation container with default styling

--- 06-react-native-navigation/App.js
+++ 06-react-native-navigation/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; // should wrap around all containers/components which we want to use the benefits of navigation package
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 
@@ -13,12 +14,17 @@ import CategoriesScreen from './screens/CategoriesScreen';
     3. expo install @react-navigation/native-stack
 */
 
+const Stack = createNativeStackNavigator(); // creates an Object (a stack like object of screens -> fitting name) with 2 properties, where every Object behaves as one component
+
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />{/* on statusBar we won't use navigation but on the whole app we will */}
-      <NavigationContainer>
-        <CategoriesScreen />
+      <StatusBar style='dark' />{/* on statusBar we won't use navigation but on the whole app we will */}
+      <NavigationContainer>{/* changes the app by default, nice wrapper -> adds a header and spacing for statusbar too */}
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />{/* allow us to register a screen managed by this navigator | name is unique */}
+        </Stack.Navigator>
+        {/* <CategoriesScreen />  we setted for this component the navigation above */}
       </NavigationContainer>
     </>
   );
