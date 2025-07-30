8528f71c30f6cd9390f12d6f283c125eeee2227d
MattheWhite <fehermatyas97@gmail.com>
2025-07-30 14:11:39 +0200

    Implement basic navigation between first and child components

--- 06-react-native-navigation/screens/CategoriesScreen.js
+++ 06-react-native-navigation/screens/CategoriesScreen.js
@@ -3,17 +3,24 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
+function CategoriesScreen({ navigation }) { // this prop 'navigation' is provided only for components which are Stack.Screen components
+  // move every function inside the component function so everyone access the props, no .bind() needed
 
-// optional, outsource here for cleaner code
-function renderCategoryItem(itemData) {
-  function pressHandler() {}
-  
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />
-  );
-}
+  // optional, outsource here for cleaner code
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview"); // provided by React Navigation, target page name is added in Stack.Screen component
+    }
 
-function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
