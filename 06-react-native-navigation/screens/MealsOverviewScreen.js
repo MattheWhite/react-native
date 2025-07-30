fba82fd3f17ca55766bb5d45b464fc057ee3031d
MattheWhite <fehermatyas97@gmail.com>
2025-07-30 16:22:56 +0200

    Start to implement more detailed MealOverviewScreen component

--- 06-react-native-navigation/screens/MealsOverviewScreen.js
+++ 06-react-native-navigation/screens/MealsOverviewScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native"; // hook - alternative way for 'route' parameter
 
 import { MEALS } from "../data/dummy-data";
@@ -7,9 +7,22 @@ function MealsOverviewScreen({ navigation, route }) {
   // const routeObj = useRoute(); - then routeObj.params . . . | this holds additional routing information
   const categoryId = route.params.categoryId; // extracts params from the passed Object in Stack.Screen
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0; // filter if meal contains the category
+  });
+
+  function renderMealItem(itemData) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Meals Overview Screen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
