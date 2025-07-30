694289111261ece05fbe799ebc76d92793a3097b
MattheWhite <fehermatyas97@gmail.com>
2025-07-30 16:09:58 +0200

    Add alternative way working with route datas

--- 06-react-native-navigation/screens/MealsOverviewScreen.js
+++ 06-react-native-navigation/screens/MealsOverviewScreen.js
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native"; // hook - alternative way for 'route' parameter
+
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ navigation, route }) {
+  // const routeObj = useRoute(); - then routeObj.params . . . | this holds additional routing information
   const categoryId = route.params.categoryId; // extracts params from the passed Object in Stack.Screen
 
   return (
