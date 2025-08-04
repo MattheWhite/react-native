4adad717c3c8cd04e6e6273e32e8991c09ea2b87
MattheWhite <fehermatyas97@gmail.com>
2025-08-04 17:13:42 +0200

    Add styling and explanation for MealItem components

--- 06-react-native-navigation/components/MealItem.js
+++ 06-react-native-navigation/components/MealItem.js
@@ -1,11 +1,11 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 function MealItem({ title, imageUrl }) { // dummy meal data has these fields which we can destruct
   return (
     <View>
       <Pressable>
         <View>
-        <Image source={{uri: imageUrl}} />
+        <Image source={{uri: imageUrl}} />{/* if the source from the web you have to define the width-height for the image */}
         <Text>{title}</Text>
         </View>
       </Pressable>
@@ -14,3 +14,10 @@ function MealItem({ title, imageUrl }) { // dummy meal data has these fields whi
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200
+  }
+});
