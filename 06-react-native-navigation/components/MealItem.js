8842ae162ce2c28430b7c4b67b8eabefe16b3a9b
MattheWhite <fehermatyas97@gmail.com>
2025-08-05 10:43:07 +0200

    Add more styling for MealItem component

--- 06-react-native-navigation/components/MealItem.js
+++ 06-react-native-navigation/components/MealItem.js
@@ -2,17 +2,17 @@ import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) { // dummy meal data has these fields which we can destruct after we passing it as props in MealsOverviewScreen
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
         <View>
           <Image source={{ uri: imageUrl }} style={styles.image} />
           {/* if the source from the web you have to define the width-height for the image */}
           <Text style={styles.title}>{title}</Text>
         </View>
-        <View>
-          <Text>{duration}minutes</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}minutes</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
       </Pressable>
     </View>
@@ -22,6 +22,12 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) { //
 export default MealItem;
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: 'hidden',
+    backgroundColor: 'white'
+  },
   image: {
     width: "100%",
     height: 200,
@@ -30,5 +36,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+    margin: 8 //padding would work here too
   },
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8, //margin would work here too,
+    justifyContent: 'center'
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12
+  }
 });
