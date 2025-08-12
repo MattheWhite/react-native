abb7b220888be2f943a465f52fc12ae5f7bcce8f
MattheWhite <fehermatyas97@gmail.com>
2025-08-12 17:14:15 +0200

    Add explanation for initialRouteName option

--- 06-react-native-navigation-extra/App.js
+++ 06-react-native-navigation-extra/App.js
@@ -10,7 +10,7 @@ const Drawer = createDrawerNavigator(); // Drawer object
 export default function App() {
     // Stack object
   return <NavigationContainer>
-    <Drawer.Navigator screenOptions={{}}>{/* the screenOptions available too | use for the screens in the navigator */}
+    <Drawer.Navigator screenOptions={{}} >{/* the screenOptions available too | use for the screens in the navigator  |  initialRouteName='User'  -> not the top most Screen will be the default starting screen */}
       <Drawer.Screen name='Welcome' component={WelcomeScreen} />{/* just like with Stack type Navigator */}
       <Drawer.Screen name='User' component={UserScreen} />
     </Drawer.Navigator>
