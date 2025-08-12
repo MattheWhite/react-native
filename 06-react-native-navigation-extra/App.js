d3b52a2ba4956e782f06acfa991f1c6bb3db7c46
MattheWhite <fehermatyas97@gmail.com>
2025-08-12 16:29:33 +0200

    Install all necessary packages for DRAWER type navigation, add explanation in App.js

--- 06-react-native-navigation-extra/App.js
+++ 06-react-native-navigation-extra/App.js
@@ -3,3 +3,11 @@ import { Text } from 'react-native';
 export default function App() {
   return <Text>Todo...</Text>;
 }
+
+/*
+INSTALL DRAWER TYPE NAVIGATOR
+
+1. npm install @react-navigation/drawer
+2. expo install react-native-gesture-handler react-native-neanimated
+
+*/
