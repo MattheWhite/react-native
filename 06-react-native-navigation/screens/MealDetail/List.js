9b91f6cef1d12604f26fc67cfa78f012e89b040e
MattheWhite <fehermatyas97@gmail.com>
2025-08-12 11:50:51 +0200

    Extend List.js component implementation

--- 06-react-native-navigation/screens/MealDetail/List.js
+++ 06-react-native-navigation/screens/MealDetail/List.js
@@ -2,12 +2,25 @@ import { Text, View, StyleSheet } from "react-native";
 
 function List({ data }) {
   return data.map((dataPoint) => (
-    <View>
-      <Text key={dataPoint}>{dataPoint}</Text>
+    <View key={dataPoint} style={styles.listItem}>
+      <Text style={styles.itemText}>{dataPoint}</Text>
     </View>
   ));
 }
 
 export default List;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listItem: {
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginHorizontal: 12,
+    marginVertical: 4,
+    backgroundColor: "#e2b445",
+  },
+  itemText: {
+    color: "#4d3d33",
+    textAlign: "center",
+  },
+});
