ae1dfb1e44d827c28e3f015d563150eaa0604563
MattheWhite <fehermatyas97@gmail.com>
2025-08-13 11:13:58 +0200

    Modify styling only on Welcome Screen

--- 06-react-native-navigation-extra/App.js
+++ 06-react-native-navigation-extra/App.js
@@ -11,7 +11,16 @@ export default function App() {
     // Stack object
   return <NavigationContainer>
     <Drawer.Navigator screenOptions={{}} >{/* the screenOptions available too | use for the screens in the navigator  |  initialRouteName='User'  -> not the top most Screen will be the default starting screen */}
-      <Drawer.Screen name='Welcome' component={WelcomeScreen} />{/* just like with Stack type Navigator */}
+      <Drawer.Screen name='Welcome' component={WelcomeScreen} options={{/* these options only applied to this screen, if you select the other the default styling is applied for it */
+        headerStyle: {backgroundColor: '#3c0a'},
+        headerTintColor: 'white',
+        drawerLabel: 'Welcome Screen',
+        drawerActiveBackgroundColor: '#58f06c',
+        drawerActiveTintColor: 'white',
+        drawerStyle: {
+          backgroundColor: '#ccc'
+        }
+      }} />{/* just like with Stack type Navigator */}
       <Drawer.Screen name='User' component={UserScreen} />
     </Drawer.Navigator>
   </NavigationContainer>;
