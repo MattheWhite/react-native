25b8671cc50fddc4c6669003c2fd0e70a72e1067
MattheWhite <fehermatyas97@gmail.com>
2025-08-19 18:42:05 +0200

    Implement icons for navigation screens, adjust active tint color

--- 06-react-native-navigation-extra-bottomTabs/App.js
+++ 06-react-native-navigation-extra-bottomTabs/App.js
@@ -10,20 +10,31 @@ const BottomTab = createBottomTabNavigator(); //
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTab.Navigator screenOptions={{}}>
+      <BottomTab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: "#3c0"
+        }}
+      >
         <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           // if you move these styling options into the main TypeSMTH.Navigation container, then all the app screen will inherit these styling options
           options={{
-            headerStyle: { backgroundColor: "#3c0a" },
-            headerTintColor: "white",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ) /* color and size automatically adjusted by default and passed to the arrow function */,
           }}
         />
         <BottomTab.Screen
           name="User"
           component={UserScreen}
-          options={{ }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
         />
       </BottomTab.Navigator>
     </NavigationContainer>
