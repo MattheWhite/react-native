5dbf8d54e92da5888aeeadd238d02769e47d6fd6
MattheWhite <fehermatyas97@gmail.com>
2025-08-22 16:36:36 +0200

    Implement Context manipulation and value object

--- 07-app-wide-state-management-contextAPI/store/context/favourites-context.js
+++ 07-app-wide-state-management-contextAPI/store/context/favourites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavouritesContext = createContext({ // create a Context object -> JSX convention the starting uppercase
     // passing initial context value
@@ -8,9 +8,27 @@ const FavouritesContext = createContext({ // create a Context object -> JSX conv
 });
 
 function FavoritesContextProvider({ children }) { // this component later can be wrapped around and used through 'children' and interact with context
+    const [favouriteMealIds, setFavouriteMealIds] = useState([]);
     
+    function addFavourite(id) {
+        setFavouriteMealIds((currentFavouriteIds) => { // in React when updating state based on prev. state snapshot, you should pass a function which automatically revceives it
+            [...currentFavouriteIds, id];
+        });
+    }
+
+    function removeFavourite(id) {
+        setFavouriteMealIds((currentFavouriteIds) => {
+            currentFavouriteIds.filter((mealId) => mealId !== id);
+        });
+    }
+
+    const value = {
+        ids: favouriteMealIds,
+        addFavourite: addFavourite, // left-side refers to create a similar object like the context above, right-side refers to the function
+        removeFavourite: removeFavourite
+    };
     
-    return <FavouritesContext.Provider>{children}</FavouritesContext.Provider>
+    return <FavouritesContext.Provider value={value}>{children}</FavouritesContext.Provider>
 }
 
 export default FavoritesContextProvider;
