422f87c17979b67f447b2fe7fe8a2d4c680c440e
MattheWhite <fehermatyas97@gmail.com>
2025-08-22 18:47:09 +0200

    Implement a fallback screen for empty favorite screen

--- 07-app-wide-state-management-contextAPI/screens/FavoritesScreen.js
+++ 07-app-wide-state-management-contextAPI/screens/FavoritesScreen.js
@@ -1,3 +1,4 @@
+import { StyleSheet, View, Text } from "react-native";
 import { useContext } from "react";
 
 import MealsList from "../components/MealsList/MealsList";
@@ -8,8 +9,29 @@ function FavoritesScreen() {
   const favoriteMealsContext = useContext(FavouritesContext);
 
   const favoriteMeals = MEALS.filter(meal => favoriteMealsContext.ids.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.text}>You have no favorite meals yet.</Text>
+    </View>
+    );
+  }
   
   return <MealsList displayedMeals={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
