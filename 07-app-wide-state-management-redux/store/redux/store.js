b3913f649cf1ca3aa056e506a8a9b30793fb292f
MattheWhite <fehermatyas97@gmail.com>
2025-08-25 10:55:17 +0200

    Create and add store.js fiel

---
+++ 07-app-wide-state-management-redux/store/redux/store.js
@@ -0,0 +1,8 @@
+// Redux Store
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+    reducer: { // slices of data (state) and actions to change those data
+
+    }
+});
