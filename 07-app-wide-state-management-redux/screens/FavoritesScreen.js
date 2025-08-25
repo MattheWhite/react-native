7874485769d85aece2f3a5bdeebedfc83d58344c
MattheWhite <fehermatyas97@gmail.com>
2025-08-25 11:40:55 +0200

    Implement using redux state in other component

--- 07-app-wide-state-management-redux/screens/FavoritesScreen.js
+++ 07-app-wide-state-management-redux/screens/FavoritesScreen.js
@@ -1,14 +1,17 @@
 import { StyleSheet, View, Text } from "react-native";
-import { useContext } from "react";
+// import { useContext } from "react";
+import { useSelector } from "react-redux";
 
 import MealsList from "../components/MealsList/MealsList";
-import { FavouritesContext } from "../store/context/favourites-context";
+// import { FavouritesContext } from "../store/context/favourites-context";
 import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  const favoriteMealsContext = useContext(FavouritesContext);
+  // const favoriteMealsContext = useContext(FavouritesContext);
+  const favoriteMealIds = useSelector(state => state.favoriteMeals.ids);
 
-  const favoriteMeals = MEALS.filter(meal => favoriteMealsContext.ids.includes(meal.id));
+  // const favoriteMeals = MEALS.filter(meal => favoriteMealsContext.ids.includes(meal.id));
+  const favoriteMeals = MEALS.filter(meal => favoriteMealIds.includes(meal.id));
 
   if (favoriteMeals.length === 0) {
     return (
