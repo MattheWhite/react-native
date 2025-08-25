3320e360a3741ef7a12f5fa2f2a4b0774f211e95
MattheWhite <fehermatyas97@gmail.com>
2025-08-25 10:53:50 +0200

    Install redux-toolkit, add Provider, provide store to the app which is wrapped around with Provider

--- 07-app-wide-state-management-redux/App.js
+++ 07-app-wide-state-management-redux/App.js
@@ -4,12 +4,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
+import { Provider } from "react-redux";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
-import FavoritesContextProvider from "./store/context/favourites-context";
+// import FavoritesContextProvider from "./store/context/favourites-context";
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -64,13 +66,14 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
-              headerStyle: { backgroundColor: "#a72d0e" },
+              headerStyle: { backgroundColor: "#0e81a7de" },
               headerTintColor: "white",
-              contentStyle: { backgroundColor: "#dd897a" },
+              contentStyle: { backgroundColor: "#87dafd" },
             }}
           >
             <Stack.Screen
@@ -93,7 +96,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      {/* </FavoritesContextProvider> */}
+      </Provider>
     </>
   );
 }
