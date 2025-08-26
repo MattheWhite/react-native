590ac8740d463d195e205a4c96ef2b05d2b8b2f0
MattheWhite <fehermatyas97@gmail.com>
2025-08-26 10:31:36 +0200

    Create and add key components to display expenses

---
+++ 08-the-expense-tracker-app/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,10 @@
+import { FlatList } from "react-native";
+
+function ExpensesList() {
+  return (
+    /* use FlatList to dynamically render only the displayed data0 */
+    <FlatList />
+  );
+}
+
+export default ExpensesList;
---
+++ 08-the-expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { FlatList, View } from "react-native";
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$9999</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
