7656733dc8e5c50676fab49c65845d44171587f7
MattheWhite <fehermatyas97@gmail.com>
2025-09-02 16:25:37 +0200

    Calculate all expenses and display it in ExpensesSummary

--- 08-the-expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
+++ 08-the-expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
@@ -1,12 +1,14 @@
 import { FlatList, View } from "react-native";
 
 function ExpensesSummary({ expenses, periodName }) {
-    const expensesSum = expenses.reduce((sum, expense) => {});
+    const expensesSum = expenses.reduce((sum, expense) => { // reduce() -> built-in method, allows to combine multiple values in an array into a single value | automatically receives: current value, current item 
+      return sum + expense.amount;
+    }, 0); // second argument is the starting value
     
   return (
     <View>
       <Text>{periodName}</Text>
-      <Text>$9999</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>{/* toFixed(2) built-in -> only display 2 decimals */}
     </View>
   );
 }
