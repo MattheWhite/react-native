252bfb3ebcc8cc39fa14f929296ec7c0e5593e75
MattheWhite <fehermatyas97@gmail.com>
2025-10-20 09:07:52 +0200

    Add useLayoutEffect hook to modification

--- 08-the-expense-tracker-app/screens/ManageExpense.js
+++ 08-the-expense-tracker-app/screens/ManageExpense.js
@@ -1,12 +1,15 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
 function ManageExpense({ route, navigation }) { // route, navigation props we automatically get here since it is a component which loaded as a screen
   const editedExpenseId = route.params?.expenseId; // ? -> JS checks if that property exists, if not it won't 'drill' into it further more
   const isEditing = !!editedExpenseId;
 
-  navigation.setOptions({
-    title: isEditing ? 'Edit Expense' : 'Add Expense'
-  });
+  useLayoutEffect(() => { // useLayoutEffect hook to avoid flickering
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
 
   return <Text>Manage Expense Screen!</Text>;
 }
