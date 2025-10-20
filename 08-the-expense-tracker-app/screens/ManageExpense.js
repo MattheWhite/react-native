18b4754fe9024aa222dda4026af2bdc01d7f5a7e
MattheWhite <fehermatyas97@gmail.com>
2025-10-20 09:06:39 +0200

    Implement dynamic title setting based on expense id

--- 08-the-expense-tracker-app/screens/ManageExpense.js
+++ 08-the-expense-tracker-app/screens/ManageExpense.js
@@ -1,7 +1,14 @@
 import { Text } from "react-native";
 
-function ManageExpense() {
-    return <Text>Manage Expense Screen!</Text>;
+function ManageExpense({ route, navigation }) { // route, navigation props we automatically get here since it is a component which loaded as a screen
+  const editedExpenseId = route.params?.expenseId; // ? -> JS checks if that property exists, if not it won't 'drill' into it further more
+  const isEditing = !!editedExpenseId;
+
+  navigation.setOptions({
+    title: isEditing ? 'Edit Expense' : 'Add Expense'
+  });
+
+  return <Text>Manage Expense Screen!</Text>;
 }
 
 export default ManageExpense;
