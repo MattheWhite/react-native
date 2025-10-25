2a962f075dcacffc755f5f3efb6ecbeef2ca1246
MattheWhite <fehermatyas97@gmail.com>
2025-10-25 19:49:50 +0200

    Add styling dynamically to generic UI button component

--- 08-the-expense-tracker-app/components/ExpensesOutput/UI/Button.js
+++ 08-the-expense-tracker-app/components/ExpensesOutput/UI/Button.js
@@ -1,11 +1,14 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-function Button({ children, onPress }) {
+import { GlobalStyles } from "../../../constants/styles";
+
+// REUSABLE AND CONFIGURABLE BUTTON UI ELEM
+function Button({ children, onPress, mode, style }) {
   return (
-    <View>
+    <View style={style}>
       <Pressable onPress={onPress}>
-        <View>
-          <Text>{children}</Text>
+        <View style={[styles.button, mode === 'flat' && styles.flat]}>
+          <Text style={[styles.buttonText, mode === 'flat' && styles.flatText]}>{children}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +16,26 @@ function Button({ children, onPress }) {
 }
 
 export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: 'center'
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4
+  }
+});
