289a4204513964ab1440824a7a0f1fd135e82659
MattheWhite <fehermatyas97@gmail.com>
2025-10-25 19:41:53 +0200

    Create and add Button generic UI component

---
+++ 08-the-expense-tracker-app/components/ExpensesOutput/UI/Button.js
@@ -0,0 +1,15 @@
+import { Pressable, Text, View } from "react-native";
+
+function Button({ children, onPress }) {
+  return (
+    <View>
+      <Pressable onPress={onPress}>
+        <View>
+          <Text>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default Button;
