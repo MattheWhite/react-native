e1586a0463a26f0df5be4088ec16b2e399cdbacb
MattheWhite <fehermatyas97@gmail.com>
2025-10-27 16:53:37 +0100

    Further implementation for ExpensesContext to manage data through the app

--- 08-the-expense-tracker-app/store/expense-context.js
+++ 08-the-expense-tracker-app/store/expense-context.js
@@ -1,5 +1,20 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-export const expensesContext = createContext({
-    
+export const ExpensesContext = createContext({
+    expenses: [],
+    addExpense: ({description, amount, date}) => {},
+    deleteExpense: (id) => {},
+    updateExpense: (id, {description, amount, date}) => {}
 });
+
+function expensesReducer(state, action) { // state, action -> received automatically provided by RN
+
+};
+
+function ExpensesContextProvider({ children }) {
+    useReducer();
+    
+    return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+
+export default ExpensesContextProvider;
