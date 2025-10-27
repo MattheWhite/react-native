f56ea36d76468a8a4295b136546e64ee0db6cf41
MattheWhite <fehermatyas97@gmail.com>
2025-10-27 16:59:14 +0100

    Further implementation using reducer hook and dispatch

--- 08-the-expense-tracker-app/store/expense-context.js
+++ 08-the-expense-tracker-app/store/expense-context.js
@@ -1,20 +1,30 @@
 import { createContext, useReducer } from "react";
 
 export const ExpensesContext = createContext({
-    expenses: [],
-    addExpense: ({description, amount, date}) => {},
-    deleteExpense: (id) => {},
-    updateExpense: (id, {description, amount, date}) => {}
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
 });
 
 function expensesReducer(state, action) { // state, action -> received automatically provided by RN
-
-};
+  switch (action.type) {
+    case "ADD":
+    case "UPDATE":
+    case "DELETE":
+    default:
+      return state;
+  }
+}
 
 function ExpensesContextProvider({ children }) {
-    useReducer();
-    
-    return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const [expensesState, dispatch] = useReducer(expensesReducer);
+
+  function addExpense({ expenseData }) {
+    dispatch();
+  }
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
 }
 
 export default ExpensesContextProvider;
