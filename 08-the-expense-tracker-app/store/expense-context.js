7cc6b880e18d54f6791ee6725a53d171ce642671
MattheWhite <fehermatyas97@gmail.com>
2025-10-31 15:31:39 +0100

    Move dummy data into expense-context component

--- 08-the-expense-tracker-app/store/expense-context.js
+++ 08-the-expense-tracker-app/store/expense-context.js
@@ -1,5 +1,44 @@
 import { createContext, useReducer } from "react";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2024-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.99,
+    date: new Date("2025-01-05"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2025-1-19"),
+  },
+  {
+    id: "e4",
+    description: "Book",
+    amount: 14.99,
+    date: new Date("2024-9-26"),
+  },
+  {
+    id: "e5",
+    description: "Another Book",
+    amount: 17.59,
+    date: new Date("2025-2-26"),
+  },
+  {
+    id: "e6",
+    description: "Porsche 911 992 GT3-R Cup II",
+    amount: 379.99,
+    date: new Date("2025-3-6"),
+  },
+];
+
 export const ExpensesContext = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
@@ -20,7 +59,7 @@ function expensesReducer(state, action) { // state, action -> received automatic
 }
 
 function ExpensesContextProvider({ children }) {
-  const [expensesState, dispatch] = useReducer(expensesReducer);
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES); // passing DUMMY_EXPENSES as the default value in case the first time this reducer executed, as an initial value | then it will get the existed state
 
   function addExpense(expenseData) {
     dispatch({ type: "ADD", payload: expenseData }); // type because we extract action.type in expensesReducer switch-case | the value we dispatch here is the 'action' parameter for expensesReducer what is made available by RN
