6efb8a230443af204304e3edbdfa1091db68d62f
MattheWhite <fehermatyas97@gmail.com>
2025-11-03 08:11:43 +0100

    Styling the input screen

--- 09-handling-user-input/components/ManageExpense/Input.js
+++ 09-handling-user-input/components/ManageExpense/Input.js
@@ -1,10 +1,34 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function Input({ label, textInputConfig }) { // instead a never ending list of props, use an object with the appropriate keys and simply pass it, automatically will be evaluated
-  return <View>
-    <Text>{label}</Text>
-    <TextInput {...textInputConfig} /> {/* keyboardType={type} maxLength={maxLength}  -> instead we continue add more and more props, we simply USE SPREAD OPERATOR */}
-  </View>;
+function Input({ label, textInputConfig }) {
+  // instead a never ending list of props, use an object with the appropriate keys and simply pass it, automatically will be evaluated
+  return (
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={styles.input} {...textInputConfig} />{" "}
+      {/* keyboardType={type} maxLength={maxLength}  -> instead we continue add more and more props, we simply USE SPREAD OPERATOR */}
+    </View>
+  );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18
+  },
+});
