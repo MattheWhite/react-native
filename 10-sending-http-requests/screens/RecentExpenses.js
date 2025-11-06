e67d7b02c51bc963b1193f3dc3d7b30ea5f8d713
MattheWhite <fehermatyas97@gmail.com>
2025-11-06 11:37:50 +0100

    Comment out the context approach, fetching existing data from Firebase

--- 10-sending-http-requests/screens/RecentExpenses.js
+++ 10-sending-http-requests/screens/RecentExpenses.js
@@ -1,14 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expense-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
-    const expensesCtx = useContext(ExpensesContext); // making available data from newly created context provider for ExpenseOutput
+    // const expensesCtx = useContext(ExpensesContext); // making available data from newly created context provider for ExpenseOutput
 
-    const recentExpenses = expensesCtx.expenses.filter((expense) => {
-        const today = new Date();
+    const [fetchedExpenses, setFetchedExpenses] = useState([]);
+
+    useEffect(() => { // NEVER TURN THE EFFECT FUNCTION ITSELF INTO ASYNC
+      async function getExpenses() { // make a helper function as async
+        const expenses = await fetchExpenses();
+        setFetchedExpenses(expenses);
+      }
+
+      getExpenses();
+    }, []);
+    
+    // const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const recentExpenses = fetchedExpenses.filter((expense) => {
+    const today = new Date();
         const date7DaysAgo = getDateMinusDays(today, 7);
         return (expense.date >= date7DaysAgo) && (expense.date <= today); // rule out the future
     });
--- 10-sending-http-requests/util/http.js
+++ 10-sending-http-requests/util/http.js
@@ -9,3 +9,20 @@ export function storeExpense(expenseData) {
   );
 }
 
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+// console.log(response.data);
+  const expenses = [];
+  for (const key in response.data) {
+    if (!Object.hasOwn(response.data, key)) continue;
+    const element = response.data[key];
+    const expenseObj = {
+      id: key, // this is the unique ID Firebase generates for the entry
+      amount: element.amount,
+      date: new Date(element.date),
+      description: element.description,
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
+}
