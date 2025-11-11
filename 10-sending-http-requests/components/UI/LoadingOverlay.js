12af31adcb0a6b0e13ab718e02d92fea11fdd44f
MattheWhite <fehermatyas97@gmail.com>
2025-11-11 17:33:37 +0100

    Further styling for loadspinner

--- 10-sending-http-requests/components/UI/LoadingOverlay.js
+++ 10-sending-http-requests/components/UI/LoadingOverlay.js
@@ -1,11 +1,22 @@
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function LoadingOverlay() {
   return (// ActivityIndicator -> built-in RN loading spinner
-    <View>
+    <View style={styles.container}>
       <ActivityIndicator size={"large"} color="white" />
     </View>
   );
 }
 
 export default LoadingOverlay;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
