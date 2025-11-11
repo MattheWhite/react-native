ac82e8bd92d5d18bfd691e1d51aa495f96364c16
MattheWhite <fehermatyas97@gmail.com>
2025-11-11 17:37:01 +0100

    Format recent expenses component and readd useState hook

--- 10-sending-http-requests/screens/RecentExpenses.js
+++ 10-sending-http-requests/screens/RecentExpenses.js
@@ -6,28 +6,30 @@ import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
-    const expensesCtx = useContext(ExpensesContext); // making available data from newly created context provider for ExpenseOutput
+  const [isFetching, setIsFetching] = useState(true);// true -> we initially always will be fetching data
+  
+  const expensesCtx = useContext(ExpensesContext); // making available data from newly created context provider for ExpenseOutput
 
-    // const [fetchedExpenses, setFetchedExpenses] = useState([]); -> modify data handling back to context to avoid redundant http requests, e.g. adding an expense no sense to send to the server than fetching it
+  // const [fetchedExpenses, setFetchedExpenses] = useState([]); -> modify data handling back to context to avoid redundant http requests, e.g. adding an expense no sense to send to the server than fetching it
 
-    useEffect(() => { // NEVER TURN THE EFFECT FUNCTION ITSELF INTO ASYNC
-      async function getExpenses() { // make a helper function as async
-        const expenses = await fetchExpenses();
-        // setFetchedExpenses(expenses);
-        expensesCtx.setExpenses(expenses);
-      }
+  useEffect(() => { // NEVER TURN THE EFFECT FUNCTION ITSELF INTO ASYNC
+    async function getExpenses() { // make a helper function as async
+      const expenses = await fetchExpenses();
+      // setFetchedExpenses(expenses);
+      expensesCtx.setExpenses(expenses);
+    }
 
-      getExpenses();
-    }, []);
-    
-    // const recentExpenses = fetchedExpenses.filter((expense) => {
-    const recentExpenses = expensesCtx.expenses.filter((expense) => {
-    const today = new Date();
-        const date7DaysAgo = getDateMinusDays(today, 7);
-        return (expense.date >= date7DaysAgo) && (expense.date <= today); // rule out the future
-    });
+    getExpenses();
+  }, []);
+  
+  // const recentExpenses = fetchedExpenses.filter((expense) => {
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  const today = new Date();
+      const date7DaysAgo = getDateMinusDays(today, 7);
+      return (expense.date >= date7DaysAgo) && (expense.date <= today); // rule out the future
+  });
 
-    return <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' fallbackText="No expenses registered for the last 7 days." />;
+  return <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' fallbackText="No expenses registered for the last 7 days." />;
 }
 
 export default RecentExpenses;
