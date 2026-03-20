2c0dfc6cbba1b6816eced03d3d7e02b95f713026
MattheWhite <fehermatyas97@gmail.com>
2026-03-20 13:16:00 +0100

    Implement same error handling for signup method

--- 11-user-authentication/screens/SignupScreen.js
+++ 11-user-authentication/screens/SignupScreen.js
@@ -1,22 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Alert } from "react-native";
 
-import AuthContent from '../components/Auth/AuthContent';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { createUser } from './util/auth';
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "./util/auth";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  async function signupHandler({email, password}) {
+  async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user, check your input and try again later!",
+      );
+    }
     setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
-    return <LoadingOverlay message={"Creating User..."} />
+    return <LoadingOverlay message={"Creating User..."} />;
   }
-  
+
   return <AuthContent onAuthenticate={signupHandler} />;
 }
 
