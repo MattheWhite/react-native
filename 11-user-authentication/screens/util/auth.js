13fe875ef024b0a041382dc08abc21620d21534b
MattheWhite <fehermatyas97@gmail.com>
2026-03-20 12:48:29 +0100

    Add descriptive comment for the shared API

--- 11-user-authentication/screens/util/auth.js
+++ 11-user-authentication/screens/util/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function createUser(email, password) {
-    const API_KEY = "AIzaSyDmf1Vh-rF9HHCfi-1uYnsEFLFVzop7SbI";
+    const API_KEY = "AIzaSyDmf1Vh-rF9HHCfi-1uYnsEFLFVzop7SbI"; // restricted public API key, no production vulnerability!
     
   const response = await axios.post(
     `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
