c0b51291243605e7132e99da0cd26a9b1cd724de
MattheWhite <fehermatyas97@gmail.com>
2026-03-20 12:56:40 +0100

    Implement both signUp and signIn functionality in auth.js

--- 11-user-authentication/screens/util/auth.js
+++ 11-user-authentication/screens/util/auth.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
-export async function createUser(email, password) {
-    const API_KEY = "AIzaSyDmf1Vh-rF9HHCfi-1uYnsEFLFVzop7SbI"; // restricted public API key, no production vulnerability!
-    
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
+async function authenticate(mode, email, password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    const response = await axios.post(url, {
         email: email,
         password: password,
         returnSecureToken: true
-    }
-  );
+    });
+
+    return response;
+}
+
+export async function createUser(email, password) {
+    await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+    await authenticate('signInWithPassword', email, password);
 }
