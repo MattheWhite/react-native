79f69cb14b701625a96977a9ed76d4dae88db5f9
MattheWhite <fehermatyas97@gmail.com>
2026-03-20 13:10:27 +0100

    Implement Login screen waiting

--- 11-user-authentication/screens/util/auth.js
+++ 11-user-authentication/screens/util/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 async function authenticate(mode, email, password) {
+    const API_KEY = "AIzaSyDmf1Vh-rF9HHCfi-1uYnsEFLFVzop7SbI"; // restricted public API key, no production vulnerability!
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
     const response = await axios.post(url, {
         email: email,
@@ -8,7 +9,7 @@ async function authenticate(mode, email, password) {
         returnSecureToken: true
     });
 
-    return response;
+    console.log(response.data);
 }
 
 export async function createUser(email, password) {
