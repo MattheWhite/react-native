f136dfa39c4d3a62c1ed03e81e8ee2625fe72b58
MattheWhite <fehermatyas97@gmail.com>
2026-03-23 15:45:02 +0100

    Add a basic data get endpoint for firebase Realtime Database on Welcome Screen

--- 11-user-authentication/screens/WelcomeScreen.js
+++ 11-user-authentication/screens/WelcomeScreen.js
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  useEffect(() => {
+    // execute this effect right after the component first loaded
+    axios.get(
+      "https://react-native-11-default-rtdb.firebaseio.com/message.json",
+    ).then((response) => {
+      setFetchedMessage(response.data);
+    });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +28,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
