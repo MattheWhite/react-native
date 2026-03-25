bf3a884d4d5493da19eb608bf1ccd275afaa520a
MattheWhite <fehermatyas97@gmail.com>
2026-03-25 15:21:49 +0100

    Add header icon to navigate and props to configure it, define default export in 2 screen

--- 12-native-device-features/src/app/_layout.tsx
+++ 12-native-device-features/src/app/_layout.tsx
@@ -1,14 +1,28 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+import IconButton from "../components/UI/IconButton";
+
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="dark" />
       <Stack>
-        <Stack.Screen name="index" options={{ title: "AllPlaces" }} />
-      </Stack>
-      ;
+        <Stack.Screen
+          name="index"
+          options={({ navigation }) => ({ // with {} braces, this returns a whole object so we can use automatically passed navigation prop to handle navigation, title changed for header not show 'index'
+            title: "AllPlaces",
+            headerRight: ({ tintColor }) => (
+              <IconButton
+                icon="add"
+                size={24}
+                color={tintColor}
+                onPress={() => navigation.navigate("AddPlace")}
+              />
+            ),
+          })}
+        />
+      </Stack>;
     </>
   );
 }
