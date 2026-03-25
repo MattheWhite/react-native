cf153f0eb0ea25203520a544edb21ba74124d5e9
MattheWhite <fehermatyas97@gmail.com>
2026-03-25 14:35:16 +0100

    Rearrange the structure to match newer file based routing

--- 12-native-device-features/src/app/index.tsx
+++ 12-native-device-features/src/app/index.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// import AllPlaces from "./screens/AllPlaces";
+// import AddPlace from "./screens/AddPlace";
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+  //   return (
+    //     <>
+    //       <StatusBar style="dark" />
+    //       <NavigationContainer>
+    //         <Stack.Navigator>
+    //           <Stack.Screen name="AllPlaces" component={AllPlaces} />
+    //           <Stack.Screen name="AddPlace" component={AddPlace} />
+    //         </Stack.Navigator>
+    //       </NavigationContainer>
+    //     </>
+    //   );
+    // }
+
+
+
+
+// --  file based routing (newer) | above is the old way  --
+//     this will be the main '/' route (landing page) -> _layout.tsx will handle this screen as 'index' since the name
+//     the routes are '/<filename>' for the components with the <filename>.tsx inside the /app folder, no need for /screens folder -> otherwise the route would be /screens/AllPlaces e.g.
+import AllPlaces from "./AllPlaces";
+export default AllPlaces;
