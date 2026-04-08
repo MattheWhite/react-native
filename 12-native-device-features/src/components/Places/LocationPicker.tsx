62d2787a29dff360eaadee0f7fb704dfc4c3fa55
MattheWhite <fehermatyas97@gmail.com>
2026-04-08 17:48:36 +0200

    Implement permission fetching and locaiton fetching completely

--- 12-native-device-features/src/components/Places/LocationPicker.tsx
+++ 12-native-device-features/src/components/Places/LocationPicker.tsx
@@ -1,15 +1,35 @@
-import { StyleSheet, View } from "react-native";
-import { getCurrentPositionAsync } from "expo-location";
+import { Alert, StyleSheet, View } from "react-native";
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location";
 
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "@/constants/colors";
 
 function LocationPicker() {
-  async function verifyPermissions() { // get permission for location
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions(); // React rule -> hooks only can be used on TOP-LEVEL component funciton
 
+  async function verifyPermissions() { // get permission for location
+    if (locationPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app.",
+      );
+      return false;
+    }
+    return true;
   }
 
   async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const location = await getCurrentPositionAsync(); // can a config object {} passed to the async call -> e.g. accuracy setting
     console.log(location);
   }
