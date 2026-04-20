cb0475d2c840e82f1bd4e17e60058be4f0820e1f
MattheWhite <fehermatyas97@gmail.com>
2026-04-20 17:35:36 +0200

    Export the util function

--- 12-native-device-features/src/components/util/location.js
+++ 12-native-device-features/src/components/util/location.js
@@ -1,6 +1,6 @@
 const GOOGLE_API_KEY = "XYZ";
 
-function getMapPreview(latitude, longitude) {
+export function getMapPreview(latitude, longitude) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&mapType=roadmap&markers=color:red%7Clabel:S%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
 }
