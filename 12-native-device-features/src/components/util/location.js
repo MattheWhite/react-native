8e1eb6120f5b8aaf399cd74f86a1b5c057bd5d52
MattheWhite <fehermatyas97@gmail.com>
2026-04-21 10:43:29 +0200

    Add explanation comment line

--- 12-native-device-features/src/components/util/location.js
+++ 12-native-device-features/src/components/util/location.js
@@ -1,5 +1,8 @@
 const GOOGLE_API_KEY = "XYZ";
 
+
+// should use your own Google account -> GCP -> Google Maps API Key
+
 export function getMapPreview(latitude, longitude) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&mapType=roadmap&markers=color:red%7Clabel:S%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
