95a24dbf51916f4be2eabbbc713994ce9f289b84
MattheWhite <fehermatyas97@gmail.com>
2026-04-23 11:45:11 +0200

    Finish geocoding api call function

--- 12-native-device-features/src/components/util/location.js
+++ 12-native-device-features/src/components/util/location.js
@@ -12,5 +12,13 @@ export function getMapPreview(latitude, longitude) {
 
 export async function getAddress(latitude, longitude) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
-  await fetch();
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch address!");
+  }
+
+  const data = await response.json();
+  const address = data.results[0].formatted_address; // official docs gives this field
+  return address;
 }
