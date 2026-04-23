b10f7dc883039ad5171f279378ad5425724ec678
MattheWhite <fehermatyas97@gmail.com>
2026-04-23 11:59:23 +0200

    Add documentation for both helper util function and finish implementation for geocoding

--- 12-native-device-features/src/components/util/location.js
+++ 12-native-device-features/src/components/util/location.js
@@ -1,15 +1,24 @@
 const GOOGLE_API_KEY = "XYZ";
 
-
-// should use your own Google account -> GCP -> Google Maps API Key
-
+/**
+ * Should use your own Google account -> GCP -> Google Maps API Key
+ * 
+ * @param {*} latitude 
+ * @param {*} longitude 
+ * @returns image preview URL with your API KEY included
+ */
 export function getMapPreview(latitude, longitude) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&mapType=roadmap&markers=color:red%7Clabel:S%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
 }
 
-// should use your own Google Cloud account -> GCP -> geocoding API
-
+/**
+ * Should use your own Google Cloud account -> GCP -> geocoding API
+ * 
+ * @param {*} latitude
+ * @param {*} longitude 
+ * @returns human readable address from the selected coordinates 
+ */
 export async function getAddress(latitude, longitude) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   const response = await fetch(url);
