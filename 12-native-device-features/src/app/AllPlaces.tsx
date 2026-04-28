e9bda837d1723ce95a7c464c4237c337048734a5
MattheWhite <fehermatyas97@gmail.com>
2026-04-28 12:57:47 +0200

    Modify AlLPlaces.tsx component to renter places items

--- 12-native-device-features/src/app/AllPlaces.tsx
+++ 12-native-device-features/src/app/AllPlaces.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused, useRoute } from "@react-navigation/native";
 
 function AllPlaces() {
-  return <PlacesList />;
+  const route = useRoute();
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces(currentPlaces => [...currentPlaces, route.params.place]); // route.params.place -> because in AddPlace we send the data on this field
+    }
+  }, [isFocused, route]);
+  
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
