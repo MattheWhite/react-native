a984c2c9194a246455525ad6cdafdd578901dc74
MattheWhite <fehermatyas97@gmail.com>
2026-04-30 14:52:14 +0200

    Initialize and handling SQLite database using SQLiteProvider in _layout

--- 12-native-device-features/src/app/_layout.tsx
+++ 12-native-device-features/src/app/_layout.tsx
@@ -1,38 +1,42 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SQLiteProvider } from 'expo-sqlite';
 
 import IconButton from "../components/UI/IconButton";
 import { Colors } from "../../constants/colors";
+import { init } from "../components/util/database";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="dark" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: Colors.primary500 },
-          headerTintColor: Colors.gray700,
-          contentStyle: { backgroundColor: Colors.gray700 },
-          unmountOnBlur: false // Keeps screen mounted when navigating away, specially with expo-routing occurs this bug
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          // with () braces, this OPTIONS returns a whole object so we can use automatically passed navigation prop to handle navigation, title changed for header not show 'index'
-          options={({ navigation }) => ({
-            title: "Your Favorite Places",
-            headerRight: ({ tintColor }) => (
-              <IconButton
-                icon="add"
-                size={24}
-                color={tintColor}
-                onPress={() => navigation.navigate("AddPlace")}
-              />
-            ),
-          })}
-        />
-        <Stack.Screen name="Map" />{/* no need for component={Map} like traditional React Navigation setup, expo-router will find the component based on file structure under app/ folder */}
-      </Stack>
+      <SQLiteProvider databaseName="places.db" onInit={init}>
+        <StatusBar style="dark" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+            unmountOnBlur: false // Keeps screen mounted when navigating away, specially with expo-routing occurs this bug
+          }}
+        >
+          <Stack.Screen
+            name="index" // navigate automatically to index.tsx because modern filebased expo-routing
+            // with () braces, this OPTIONS returns a whole object so we can use automatically passed navigation prop to handle navigation, title changed for header not show 'index'
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen name="Map" />{/* no need for component={Map} like traditional React Navigation setup, expo-router will find the component based on file structure under app/ folder */}
+        </Stack>
+      </SQLiteProvider>
     </>
   );
 }
--- 12-native-device-features/src/components/util/database.js
+++ 12-native-device-features/src/components/util/database.js
@@ -1,10 +1,13 @@
 import * as SQLite from 'expo-sqlite'; // i will use everywhere Asyncronous execution
 // The legacy openDatabese(), transaction(), executeSql() and so on... callback-based APIs are deprecated in favor of async/await methods like runAsync(), getAllAsync(), and getFirstAsync(), which offer better performance and type safety. 
 
+/*
+When you use (in _layout.tsx modern way) <SQLiteProvider databaseName="places.db" onInit={init}>, it internally manages the database connection and automatically passes the database instance to your init function as a parameter.
 
 const database = await SQLite.openDatabaseAsync('places.db'); // create if not exists, open if it does
+*/
 
-export async function init() {// sends the initial base database structure, must runned at least once
+export async function init(database) {// sends the initial base database structure, must runned at least once
     try {
         await database.withTransactionAsync( async () => { // transaction object automatically passed by expo-sqlite package, but i use the modern way, no longer needed
             await database.execAsync(`CREATE TABLE IF NOT EXISTS places (
