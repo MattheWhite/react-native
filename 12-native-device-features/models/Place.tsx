d2872179f3fcf68e4d437981bcc61777134cba99
MattheWhite <fehermatyas97@gmail.com>
2026-06-01 17:51:38 +0200

    Modify Place model class, id is now initialized

--- 12-native-device-features/models/Place.tsx
+++ 12-native-device-features/models/Place.tsx
@@ -1,9 +1,10 @@
 export class Place {
-  constructor(title, imageUri, location) {
+  constructor(title, imageUri, location, id) {
     this.title = title;
     this.imageUri = imageUri;
     this.address = location.address;
     this.location = {lat: location.lat, lng: location.lng}; //  { latitude: number, longitude: number }
-    this.id = new Date().toString() + Math.random().toString();
+    // this.id = new Date().toString() + Math.random().toString();
+    this.id = id; // will be set at last by database and when it is fetched, at object initialization it will be received now
   }
 }
