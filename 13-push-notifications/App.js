98ecc77c9dc9fd3bb37ba16973a61477a2bb379d
MattheWhite <fehermatyas97@gmail.com>
2026-06-08 11:49:16 +0200

    Finish implementing push notification API call and sending

--- 13-push-notifications/App.js
+++ 13-push-notifications/App.js
@@ -82,10 +82,25 @@ export default function App() {
       }
     });
   }
+
+  function sendPushNotificationHandler() {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ // convert data to JSON string format
+        to: 'xyz', // here the token would be fetched dynamically from the notification server
+        title: 'Test - sent from a device',
+        body: 'This is a TEST!'
+      })
+    });
+  }
   
   return (
     <View style={styles.container}>
       <Button title='Scedule Notification' onPress={sceduleNotificationHandler} />
+      <Button title='Send Push Notification' onPress={sendPushNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
