0e6939c9d4573f65db6e3d160cd60c40a1f9326c
MattheWhite <fehermatyas97@gmail.com>
2026-06-08 10:15:01 +0200

    iImplement setNotificationHandler

--- 13-push-notifications/App.js
+++ 13-push-notifications/App.js
@@ -2,6 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
+// in this main App.js file  ->  ensures this code will run when the app starts 100%
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowBanner: true // shouldShowAlert -> deprecated, use this or shouldShowList
+    };
+  }
+})
+
 export default function App() {
   function sceduleNotificationHandler() { // IMPORTANT --------------------------------->>>> Expo 53 SDK eliminated push notifications with expo's sandbox credential | now developers must do a development build instead of Expo Go
     Notifications.scheduleNotificationAsync({
